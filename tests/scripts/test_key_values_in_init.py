940dbd6b2a2f3515f7b7cdc74e9a7c5a5326a238
berat Tuna KARLI <tuna.karli@synnada.ai>
2024-12-01 11:28:04 +0300

    pre-commit fix

--- tests/scripts/test_key_values_in_init.py
+++ tests/scripts/test_key_values_in_init.py
@@ -15,7 +15,7 @@
 import pytest
 
 import mithril as ml
-from mithril.models import Add, Model
+from mithril.models import Add, Linear, Model
 from mithril.utils.utils import OrderedSet
 
 
@@ -26,33 +26,36 @@ def test_directed_call_connection():
 
     info = add1(left=connection, right="right")
     left_info = info._connections["left"]
-    
+
     assert isinstance(left_info, ml.Connect)
     assert left_info.connections == OrderedSet([connection.data])
     assert isinstance(left_info.key, ml.IOKey)
-    assert left_info.key._name == None
+    assert left_info.key._name is None
     assert left_info.key._value == 1
 
 
 def test_directed_call_int():
-    add1 = Add(left = 1)
-    info = add1(left = 1, right = "right")
+    add1 = Add(left=1)
+    info = add1(left=1, right="right")
 
     assert info._connections["left"] == 1
 
 
 def test_directed_call_int_error():
-    add1 = Add(left = 1)
-    # NOTE: Since 1 == 1.0 or 1 == True, this will not 
+    add1 = Add(left=1)
+    # NOTE: Since 1 == 1.0 or 1 == True, this will not
     # raise an error if we would have give 1.0 or True.
     with pytest.raises(ValueError) as err_info:
-        add1(left = 2, right = "right")
-    assert str(err_info.value) == "Given value 2 for local key: 'left' has already being set to 1!"
+        add1(left=2, right="right")
+    assert (
+        str(err_info.value)
+        == "Given value 2 for local key: 'left' has already being set to 1!"
+    )
 
 
 def test_directed_call_str():
-    add1 = Add(left = 1)
-    info = add1(left = "in1", right = "right")
+    add1 = Add(left=1)
+    info = add1(left="in1", right="right")
 
     left_info = info._connections["left"]
 
@@ -81,7 +84,7 @@ def test_directed_call_iokey_value_not_equal():
         add1(left=iokey, right="right")
     assert str(err_info.value) == "Given IOKey for local key: 'left' is not valid!"
 
-    
+
 def test_directed_call_iokey_value_tbd():
     add1 = Add(left=1)
     iokey = ml.IOKey("in1")  # value is TBD
@@ -91,17 +94,22 @@ def test_directed_call_iokey_value_tbd():
 
     assert isinstance(left_info, ml.IOKey)
     assert left_info._name == "in1"
-    assert left_info._value == 1 # value is set to val from factory_inputs
+    assert left_info._value == 1  # value is set to val from factory_inputs
 
 
 def test_directed_call_connect_key_value_not_equal():
     add1 = Add(left=1)
     iokey = ml.IOKey("in1", value=2)  # value does not match val in factory_inputs
-    connect = ml.Connect(Add().left, key=iokey) # Dummy connection with unmatching IOKey
+    connect = ml.Connect(
+        Add().left, key=iokey
+    )  # Dummy connection with unmatching IOKey
 
     with pytest.raises(ValueError) as err_info:
         add1(left=connect)
-    assert str(err_info.value) == "Given IOKey in Connect for local key: 'left' is not valid!"
+    assert (
+        str(err_info.value)
+        == "Given IOKey in Connect for local key: 'left' is not valid!"
+    )
 
 
 def test_directed_call_connect_key_none():
@@ -153,7 +161,10 @@ def test_directed_call_extend_template():
 
     with pytest.raises(ValueError) as err_info:
         add1(left=template)
-    assert str(err_info.value) == "Multi-write detected for a valued local key: 'left' is not valid!"
+    assert (
+        str(err_info.value)
+        == "Multi-write detected for a valued local key: 'left' is not valid!"
+    )
 
 
 def test_directed_call_key_not_in_kwargs():
@@ -176,7 +187,7 @@ def test_directed_call_factory_val_tbd():
 def test_integration_call_arg_connection():
     add1 = Add(left=1)
     add2 = Add()
-    
+
     model = Model()
     model += add2(left="in1", right="in2", output="out1")
     model += add1(left=add2.left, right=add2.output, output="output")
@@ -188,10 +199,10 @@ def test_integration_call_arg_connection():
 
 
 def test_integration_call_arg_str():
-    add1 = Add(left = 1)
+    add1 = Add(left=1)
 
     model = Model()
-    model += add1(left = "in1", right = "in2")
+    model += add1(left="in1", right="in2")
 
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, data_keys=["in2"], jit=False)
@@ -199,10 +210,10 @@ def test_integration_call_arg_str():
 
 
 def test_integration_call_arg_int():
-    add1 = Add(left = 1)
+    add1 = Add(left=1)
 
     model = Model()
-    model += add1(left = 1, right = "in2", output = "output")
+    model += add1(left=1, right="in2", output="output")
 
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, data_keys=["in2"], jit=False)
@@ -210,21 +221,24 @@ def test_integration_call_arg_int():
 
 
 def test_integration_call_arg_int_not_equal():
-    add1 = Add(left = 1)
+    add1 = Add(left=1)
 
     model = Model()
     with pytest.raises(ValueError) as err_info:
-        model += add1(left = 3, right = "in2")
-    assert str(err_info.value) == "Given value 3 for local key: 'left' has already being set to 1!"
+        model += add1(left=3, right="in2")
+    assert (
+        str(err_info.value)
+        == "Given value 3 for local key: 'left' has already being set to 1!"
+    )
 
 
 def test_integration_call_arg_iokey_value_equal():
     add1 = Add(left=1)
     iokey = ml.IOKey("in1", value=1)
-    
+
     model = Model()
     model += add1(left=iokey, right="in2", output="output")
-    
+
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, data_keys=["in2"], jit=False)
     assert pm.evaluate(data={"in2": 2})["output"] == backend.array(3.0)
@@ -233,7 +247,7 @@ def test_integration_call_arg_iokey_value_equal():
 def test_integration_call_arg_iokey_value_not_equal():
     add1 = Add(left=1)
     iokey = ml.IOKey("in1", value=2)
-    
+
     model = Model()
     with pytest.raises(ValueError) as err_info:
         model += add1(left=iokey, right="in2", output="output")
@@ -243,10 +257,10 @@ def test_integration_call_arg_iokey_value_not_equal():
 def test_integration_call_arg_iokey_value_tbd():
     add1 = Add(left=1)
     iokey = ml.IOKey("in1")  # value is TBD
-    
+
     model = Model()
     model += add1(left=iokey, right="in2", output="output")
-    
+
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, data_keys=["in2"], jit=False)
     assert pm.evaluate(data={"in2": 2})["output"] == backend.array(3.0)
@@ -256,25 +270,29 @@ def test_integration_call_arg_connect_key_value_not_equal():
     add1 = Add(left=1)
     iokey = ml.IOKey("in1", value=2)
     connect = ml.Connect(Add().left, key=iokey)
-    
+
     model = Model()
     with pytest.raises(ValueError) as err_info:
         model += add1(left=connect, right="in2", output="output")
-    assert str(err_info.value) == "Given IOKey in Connect for local key: 'left' is not valid!"
+    assert (
+        str(err_info.value)
+        == "Given IOKey in Connect for local key: 'left' is not valid!"
+    )
 
 
 def test_integration_call_arg_connect_key_none():
     add1 = Add(left=1)
     add2 = Add()
     con = ml.Connect(add2.left, key=None)
-    
+
     model = Model()
     model += add2(left="in1", right="in2")
     model += add1(left=con, right=add2.output, output="output")
-    
+
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, data_keys=["in2"], jit=False)
     assert pm.evaluate(data={"in2": 2})["output"] == backend.array(4.0)
+
 
 add1 = Model() + Add()(left="left", right="right", output="output")
 # add1.set_values(left=1)
@@ -290,11 +308,10 @@ def test_integration_call_arg_connect_key_value_tbd():
     add1 = Add(left=1)
     add2 = Add()
     con = ml.Connect(add2.left, key=ml.IOKey("in1"))
-    
+
     model = Model()
     model += add2(right="in2")
     model += add1(left=con, right=add2.output, output="output")
-
 
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, data_keys=["in2"], jit=False)
@@ -305,7 +322,7 @@ def test_integration_call_arg_connect_key_value_equal():
     add1 = Add(left=1)
     add2 = Add()
     con = ml.Connect(add2.left, key=ml.IOKey(value=1))
-    
+
     model = Model()
     model += add2(right="in2")
     model += add1(left=con, right=add2.output, output="output")
@@ -318,88 +335,46 @@ def test_integration_call_arg_connect_key_value_equal():
 def test_integration_call_arg_extend_template():
     add1 = Add(left=1)
     template = ml.IOKey("in1") + ml.IOKey("in2")
-    
+
     model = Model()
     with pytest.raises(ValueError) as err_info:
         model += add1(left=template)
-    assert str(err_info.value) == "Multi-write detected for a valued local key: 'left' is not valid!"
+    assert (
+        str(err_info.value)
+        == "Multi-write detected for a valued local key: 'left' is not valid!"
+    )
 
 
 def test_integration_call_arg_key_not_in_kwargs():
     add = Add()
     model = Model()
     model += add(output="output")
-    
+
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, jit=False)
     res = pm.evaluate(params={"input": backend.array(1.0), "right": backend.array(2.0)})
     assert res["output"] == backend.array(3.0)
 
-from mithril.models import Linear
+
 def test_integration_call_arg_factory_val_tbd():
     add1 = Add()  # factory_inputs have TBD values
-    
+
     model = Model()
     model += add1(left="in1", right="in2", output="output")
-    
+
     backend = ml.TorchBackend()
     pm = ml.compile(model, backend, jit=False)
-    assert pm.evaluate(params={"in1": backend.array(1.0), "in2": backend.array(2.0)})["output"] == backend.array(3.0)
+    assert pm.evaluate(params={"in1": backend.array(1.0), "in2": backend.array(2.0)})[
+        "output"
+    ] == backend.array(3.0)
 
-
-    lin = Linear(b = [1.0])
+    lin = Linear(b=[1.0])
     lin.summary()
 
     model = Model()
     model += lin
     model.summary()
 
-
-
     model = Model()
-    model += Add(left = 1.0)
+    model += Add(left=1.0)
     model.summary()
-
-
-
-
-
-
-
-
-
-
-    # def __call__(self, **kwargs: ConnectionType) -> ExtendInfo:
-    #     for key, val in self.factory_inputs.items():
-    #         if val is not TBD:
-    #             if key not in kwargs or (con := kwargs[key]) is NOT_GIVEN:
-    #                 kwargs[key] = val
-    #             elif isinstance(con, Connection):
-    #                 kwargs[key] = Connect(con, key = IOKey(value=val))
-    #                 # TODO: Maybe we could check con's value if matches with val
-    #             elif isinstance(con, MainValueInstance) and con != val:
-    #                 raise ValueError(f"Given value {con} for local key: '{key}' has already being set to {val}!")
-    #             elif isinstance(con, str):
-    #                 kwargs[key] = IOKey(con, value=val)
-    #             elif isinstance(con, IOKey):
-    #                 if con._value is not TBD and con._value != val:
-    #                     raise ValueError(
-    #                         f"Given IOKey for local key: '{key}' is not valid!"
-    #                     )
-    #                 else:
-    #                     con._value = val
-    #             elif isinstance(con, Connect):
-    #                 if (io_key := con.key) is not None:
-    #                     if io_key._value is not TBD and io_key._value != val:
-    #                         raise ValueError(
-    #                             f"Given IOKey in Connect for local key: '{key}' is not valid!"
-    #                         )
-    #                     else:
-    #                         io_key._value = val
-    #                 else:
-    #                     con.key = IOKey(value=val)
-    #             elif isinstance(con, ExtendTemplate):
-    #                 raise ValueError(
-    #                     f"Multi-write detected for a valued local key: '{key}' is not valid!"
-    #                 )                    
-    #     return ExtendInfo(self, kwargs)
