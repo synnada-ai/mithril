5a2ca6828a44c070b7c1c53b489a5bee3bdcc564
Abdussamet Turker <152855144+aturker-synnada@users.noreply.github.com>
2024-12-16 13:43:06 +0300

    refactor: Rearrange Nano GPT Model Names for Consistency (#78)

--- examples/gpt/model.py
+++ examples/gpt/model.py
@@ -17,6 +17,7 @@ This example is equivalent of nanoGPT by karpathy.
 Torch implementation of nanoGPT: https://github.com/karpathy/nanoGPT
 """
 
+from mithril import IOKey
 from mithril.models import (
     Add,
     Arange,
@@ -27,6 +28,7 @@ from mithril.models import (
     Model,
     ScaledDotProduct,
     Size,
+    Split,
 )
 
 
@@ -35,58 +37,71 @@ def causal_attention(input_dim, num_heads, bias=True):
     if (input_dim % num_heads) != 0:
         raise ValueError("Requires input dims to be divisible by num_heads")
 
-    model = Model()
-    model += Linear(input_dim)("input", output="lq_out")
-    model += Linear(input_dim)("input", output="lk_out")
-    model += Linear(input_dim)("input", output="lv_out")
+    model = Model(name="attn")
+    model += Linear(input_dim * 3, name="c_attn")("input", output="c_attn_out")
 
     t_axes = (0, 2, 1, 3)
     shp_con = model.input.shape()  # type: ignore
     reshape_con = (shp_con[0], shp_con[1], num_heads, -1)
 
-    tq = model.lq_out.reshape(reshape_con).transpose(t_axes)  # type: ignore
-    tk = model.lk_out.reshape(reshape_con).transpose(t_axes)  # type: ignore
-    tv = model.lv_out.reshape(reshape_con).transpose(t_axes)  # type: ignore
+    model += Split(3, axis=-1)(model.c_attn_out, output="split_out")  # type: ignore
+    tq = model.split_out[0].reshape(reshape_con).transpose(t_axes)  # type: ignore
+    tk = model.split_out[1].reshape(reshape_con).transpose(t_axes)  # type: ignore
+    tv = model.split_out[2].reshape(reshape_con).transpose(t_axes)  # type: ignore
 
     model += ScaledDotProduct()(query=tq, key=tk, value=tv, output="sdp_out")
     t_sdp = model.sdp_out.transpose(t_axes).reshape(shp_con[:3])  # type: ignore
-    model += Linear(input_dim)(t_sdp)
+    model += Linear(input_dim, name="c_proj")(t_sdp)
+
     return model
+
+
+def mlp(n_embd: int):
+    block = Model(name="mlp")
+    block += Linear(n_embd * 4, name="c_fc")(input="input")
+    block += Gelu()
+    block += Linear(n_embd, name="c_proj")(output=IOKey("output"))
+
+    return block
 
 
 # Create Transformer Encoder Block.
 def create_block(dims, num_heads, mlp_dims, bias=True, eps=1e-5):
     block = Model()
-    block += LayerNorm(use_bias=bias, eps=eps)("input")
+    block += LayerNorm(use_bias=bias, eps=eps, name="ln_1")("input")
     block += causal_attention(dims, num_heads, bias)
     block += Add()("input", block.canonical_output, "add_out")
-    block += LayerNorm(use_bias=bias, eps=eps)
-    block += Linear(mlp_dims)
-    block += Gelu()
-    block += Linear(dims)
+    block += LayerNorm(use_bias=bias, eps=eps, name="ln_2")
+    block += mlp(dims)
+
     block += Add()("add_out", right=block.canonical_output)
     return block
 
 
 def create_gpt(bias, block_size, dims, mlp_dims, num_heads, num_layers, vocab_size):
     # Create Position Embedding model
-    position = Model()
-    size_model = Size(dim=1)
-    size_model.input.set_differentiable(False)
-    position += size_model("input")
-    position += Arange(start=0, step=1)
-    position += Embedding(block_size, dims)(
-        input=position.canonical_output, output="output"
+    transformer = Model(name="transformer")
+    transformer += Size(dim=1)("input")
+    transformer += Arange(start=0, step=1)
+    transformer += Embedding(name="wpe", num_embeddings=block_size, dim=dims)(
+        output="pos_out"
     )
+    transformer += Embedding(name="wte", num_embeddings=vocab_size, dim=dims)(
+        "input", output="token_out"
+    )
+    transformer += Add()("pos_out", "token_out")
+
+    blocks = Model(name="h")
+    for idx in range(num_layers):
+        block = create_block(dims, num_heads, mlp_dims)
+        block.name = f"{idx}"  # Match names with torch
+        blocks += block
+    transformer += blocks
+    transformer += LayerNorm(use_bias=bias, name="ln_f")
+
     # Create GPT
     gpt = Model()
-    gpt += Embedding(vocab_size, dims)("input", output="token_out")
-    gpt += position(input="input")
-    gpt += Add()("token_out", position.canonical_output)
-
-    for _ in range(num_layers):
-        gpt += create_block(dims, num_heads, mlp_dims)
-
-    gpt += LayerNorm(use_bias=bias)
-    gpt += Linear(vocab_size, use_bias=False)
+    gpt += transformer(input="input")
+    gpt += Linear(vocab_size, use_bias=False, name="lm_head")(output=IOKey("output"))
+    gpt.input.set_differentiable(False)  # type: ignore
     return gpt
