4b99c38e8b570db9a3f0197a0771e8885dc773b5
kberat-synnada <97015093+kberat-synnada@users.noreply.github.com>
2024-12-27 10:42:13 +0300

    Update PhysicalModel _flat_graph attribute

--- mithril/framework/physical/model.py
+++ mithril/framework/physical/model.py
@@ -519,7 +519,7 @@ class PhysicalModel(GenericDataType[DataType]):
         # Set given shapes.
         self.data_store.set_shapes(shapes)
 
-        for node in self._flat_graph.nodes.values():
+        for node in self.flat_graph.nodes.values():
             conn_data = node.model.conns.get_connection("output")
             assert conn_data is not None
             if isinstance(conn_data.metadata.data, Scalar) or (
