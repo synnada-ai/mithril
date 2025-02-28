58fb22d454762355262957e47e38f8ff59eb358a
Omer Kus <omer.kus@hubx.co>
2025-02-28 13:12:22 +0000

    multi_head_attetntion was changed

--- examples/clip/model.py
+++ examples/clip/model.py
@@ -25,49 +25,46 @@ def multi_head_attention(
 ):
     block = Model(name=name)
     assert d_model % n_head == 0, "d_model is not divisible by h"
-    queries = IOKey("queries")
+    queries = IOKey("queries",shape = (1,77,768))
+    head_dim = d_model // n_head
     B, L = queries.shape[0], queries.shape[1]
     block |= Linear(3 * d_model, name="in_proj")(queries, output="in_proj")
 
     in_proj: ml.Connection = block.in_proj
-    in_proj = in_proj.reshape((B, L, 3, -1))
-    block |= Buffer()(input=in_proj, output="in_proj_buffer")
+    # block |= (buffer_pre:=Buffer())(input=in_proj.reshape((B, L, n_head,3*head_dim)).transpose((0, 2, 1, 3)), output="in_proj_buffer")
+    in_proj = in_proj.reshape((B, L, n_head,3*head_dim)).transpose((0, 2, 1, 3)).reshape((B, L, n_head,3,-1))
+    
+    # block |= Buffer()(input=in_proj, output="in_proj_buffer")
 
     queries = (
-        in_proj[:, :, 0]
-        .reshape((B, L, -1))
+        in_proj[:, :, :,0,:]
         .reshape((B, L, n_head, -1))
-        .transpose((0, 2, 1, 3))
-    )
+    ) 
     # queries = in_proj[:,:,0].reshape((B, L,-1))
 
     keys = (
-        in_proj[:, :, 1]
-        .reshape((B, L, -1))
+        in_proj[:, :, :,1,:]
         .reshape((B, L, n_head, -1))
-        .transpose((0, 2, 1, 3))
     )
     values = (
-        in_proj[:, :, 2]
-        .reshape((B, L, -1))
+        in_proj[:, :, :,2,:]
         .reshape((B, L, n_head, -1))
-        .transpose((0, 2, 1, 3))
     )
-    block |= Buffer()(input=queries, output="query_buffer")
-    block |= Buffer()(input=keys, output="key_buffer")
-    block |= Buffer()(input=values, output="quer_proj_buffer")
+    block |= (buf_q:=Buffer())(input=queries, output="query_buffer")
+    block |= (buf_k:=Buffer())(input=keys, output="key_buffer")
+    block |= (buf_v:=Buffer())(input=values, output="value_buffer")
 
     if use_attn_mask:
-        block |= ScaledDotProduct(is_causal=False, use_attn_mask=True)(
-            queries,
-            keys,
-            values,
-            attn_mask=IOKey("mask", shape=(77, 77)),
+        block |= ScaledDotProduct(is_causal=False,use_attn_mask=True)(
+            query = queries,
+            key = keys,
+            value = values,
+            attn_mask=IOKey("mask"),
             output="attention",
         )
     else:
-        block |= ScaledDotProduct(is_causal=False)(
-            queries, keys, values, output="attention"
+        block |= ScaledDotProduct(is_causal=False,use_attn_mask = False)(
+            query = queries, key = keys, value = values, output="attention"
         )
     values_hat = block.attention.transpose((0, 2, 1, 3)).reshape((B, L, -1))
     block |= Linear(d_model, name="out_proj")(values_hat, output=IOKey("output"))
