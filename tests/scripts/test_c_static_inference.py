3353388118d22a96acda1e492611a82c45893067
emrecakmakyurdu <ecyurdu@gmail.com>
2025-03-09 18:13:33 +0300

    bugfix: update ggml codegen to support using cached results in operations

--- tests/scripts/test_c_static_inference.py
+++ tests/scripts/test_c_static_inference.py
@@ -364,3 +364,79 @@ def test_c_static_inference_5():
         assert np.allclose(c_backend.to_numpy(out), out_np)
         assert np.allclose(ggml_backend.to_numpy(out_ggml), out_np)
 
+
+def test_c_static_inference_6():
+    """
+    Test static inference support when outputs of 
+    model are used in other operation with partial static inputs
+    """
+    model = Model()
+
+    model += Add()(left="left", right="right", output="output")
+    model |=Multiply()(left="left1", right="right1", output="output2")
+    model |= Add()(left="output2", right="output", output="output3")
+    model.set_types(left=Tensor, right=Tensor, left1=Tensor, right1=Tensor)
+
+    c_backend = CBackend()
+    np_backend = NumpyBackend()
+    ggml_backend = GGMLBackend()
+    
+    left_static = np.ones((5, 5), dtype=np.float32)
+    right_static = np.ones((5, 5), dtype=np.float32)
+
+    c_pm = compile(
+        model,
+        c_backend,
+        shapes={"left1": [5, 5], "right1": [5, 5] },
+        constant_keys={
+            "left": c_backend.array(left_static),
+            "right": c_backend.array(right_static),
+        },
+        jit=False,
+        inference=True
+    )
+
+    np_pm = compile(
+        model,
+        np_backend,
+        trainable_keys={"left1", "right1"},
+        constant_keys={
+            "left": left_static,
+            "right": right_static,
+        },
+        jit=False,
+        inference=True
+    )
+
+    ggml_pm = compile(
+        model,
+        ggml_backend,
+        shapes={"left1": [5, 5], "right1": [5, 5] },
+        constant_keys={
+            "left": ggml_backend.array(left_static),
+            "right": ggml_backend.array(right_static),
+        },
+        jit=False,
+        inference=True
+    )
+    
+    # Numpy Backend
+    np_outputs = np_pm.evaluate({"left1": left_static, "right1": right_static})
+
+    # Raw C Backend
+    c_left = c_backend.array(left_static)
+    c_right = c_backend.array(right_static)
+    c_outputs = c_pm.evaluate({"left1": c_left, "right1": c_right})
+    
+    # GGML Backend
+    ggml_left1 = ggml_backend.array(left_static)
+    ggml_right1 = ggml_backend.array(right_static)
+    ggml_outputs = ggml_pm.evaluate({"left1": ggml_left1, "right1": ggml_right1})
+
+    # Assertions
+    for key in np_outputs:
+        out = c_outputs[key]
+        out_ggml = ggml_outputs[key]
+        out_np = np_outputs[key]
+        assert np.allclose(c_backend.to_numpy(out), out_np)
+        assert np.allclose(ggml_backend.to_numpy(out_ggml), out_np)
