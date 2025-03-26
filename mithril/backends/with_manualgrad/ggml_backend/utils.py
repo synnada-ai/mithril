321203970e7352a9b637e58172d24c35a69417e1
Abdussamet Turker <152855144+aturker-synnada@users.noreply.github.com>
2025-03-26 13:59:19 +0300

    feat: Add broadcast support ggml (#255)

--- mithril/backends/with_manualgrad/ggml_backend/utils.py
+++ mithril/backends/with_manualgrad/ggml_backend/utils.py
@@ -24,6 +24,12 @@ CODEGEN_CONFIG.HEADER_NAME = "ggml_backend.h"
 CODEGEN_CONFIG.ARRAY_NAME = "struct ggml_tensor"
 
 # Function configs
+CODEGEN_CONFIG.IMPLICIT_BROADCAST_OPS = {
+    "add",
+    "subtract",
+    "multiplication",
+    "divide",
+}
 CODEGEN_CONFIG.USE_OUTPUT_AS_INPUT = False
 CODEGEN_CONFIG.RETURN_OUTPUT = True
 
