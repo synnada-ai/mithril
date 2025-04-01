ba7aba1db64908c7caf3e259a448dffd3ef40364
emrecakmakyurdu <ecyurdu@gmail.com>
2025-04-01 16:43:17 +0300

    pre-commit fix

--- tests/scripts/test_pyarray_arithmetic.py
+++ tests/scripts/test_pyarray_arithmetic.py
@@ -17,6 +17,7 @@ import numpy as np
 
 import mithril as ml
 
+
 def prepare_inputs():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
@@ -26,44 +27,47 @@ def prepare_inputs():
 
     left_c = c_backend.array(left)
     right_c = c_backend.array(right)
-    
+
     left_ggml = ggml_backend.array(left)
     right_ggml = ggml_backend.array(right)
-    
-    return left, right, left_c, right_c, left_ggml, right_ggml    
+
+    return left, right, left_c, right_c, left_ggml, right_ggml
+
 
 def test_scalar_radd():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
-    
-    left, _, left_c, _, left_ggml, _  = prepare_inputs()
-    
+
+    left, _, left_c, _, left_ggml, _ = prepare_inputs()
+
     scalar = 10.0
     result_c = scalar + left_c
     result_ggml = scalar + left_ggml
     expected = scalar + left
     assert np.allclose(c_backend.to_numpy(result_c), expected)
     assert np.allclose(ggml_backend.to_numpy(result_ggml), expected)
-    
+
+
 def test_scalar_add():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
-    
-    left, _, left_c, _, left_ggml, _  = prepare_inputs()
-    
+
+    left, _, left_c, _, left_ggml, _ = prepare_inputs()
+
     scalar = 2.0
     result_c = left_c + scalar
     result_ggml = left_ggml + scalar
     expected = left + scalar
     assert np.allclose(c_backend.to_numpy(result_c), expected)
     assert np.allclose(ggml_backend.to_numpy(result_ggml), expected)
-    
+
+
 def test_pyarray_add():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
-    
+
     left, right, left_c, right_c, left_ggml, right_ggml = prepare_inputs()
-    
+
     result_c = left_c + right_c
     result_ggml = left_ggml + right_ggml
     expected = left + right
@@ -75,60 +79,63 @@ def test_scalar_rsubtract():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
 
-    left, _, left_c, _, left_ggml, _  = prepare_inputs()
-    
+    left, _, left_c, _, left_ggml, _ = prepare_inputs()
+
     scalar = 10.0
     result_c = scalar - left_c
     result_ggml = scalar - left_ggml
     expected = scalar - left
     assert np.allclose(c_backend.to_numpy(result_c), expected)
     assert np.allclose(ggml_backend.to_numpy(result_ggml), expected)
-    
+
+
 def test_scalar_subtract():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
-    
-    left, _, left_c, _, left_ggml, _  = prepare_inputs()
-    
+
+    left, _, left_c, _, left_ggml, _ = prepare_inputs()
+
     scalar = 2.0
     result_c = left_c - scalar
     result_ggml = left_ggml - scalar
     expected = left - scalar
     assert np.allclose(c_backend.to_numpy(result_c), expected)
     assert np.allclose(ggml_backend.to_numpy(result_ggml), expected)
-    
+
+
 def test_pyarray_subtract():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
-    
+
     left, right, left_c, right_c, left_ggml, right_ggml = prepare_inputs()
-    
+
     result_c = left_c - right_c
     result_ggml = left_ggml - right_ggml
     expected = left - right
     assert np.allclose(c_backend.to_numpy(result_c), expected)
     assert np.allclose(ggml_backend.to_numpy(result_ggml), expected)
-    
-    
+
+
 def test_scalar_multiply():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
-    
-    left, _, left_c, _, left_ggml, _  = prepare_inputs()
-    
+
+    left, _, left_c, _, left_ggml, _ = prepare_inputs()
+
     scalar = 2.0
     result_c = left_c * scalar
     result_ggml = left_ggml * scalar
     expected = left * scalar
     assert np.allclose(c_backend.to_numpy(result_c), expected)
     assert np.allclose(ggml_backend.to_numpy(result_ggml), expected)
-    
+
+
 def test_pyarray_multiply():
     c_backend = ml.CBackend()
     ggml_backend = ml.GGMLBackend()
-    
+
     left, right, left_c, right_c, left_ggml, right_ggml = prepare_inputs()
-    
+
     result_c = left_c * right_c
     result_ggml = left_ggml * right_ggml
     expected = left * right
