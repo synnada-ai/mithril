3ed5e099f0b657ba3034d6e027aa1a2c3c6d547b
emrecakmakyurdu <ecyurdu@gmail.com>
2025-04-01 14:08:39 +0300

    review fixes

--- mithril/cores/c/ggml/ops.py
+++ mithril/cores/c/ggml/ops.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import ctypes
+from collections.abc import Callable
 
 from ....cores.c.array import PyArray
 from ....cores.c.ggml.ggml_core import ggml_struct
@@ -22,30 +23,27 @@ __all__ = ["add", "multiplication"]
 
 
 def add(left: PyArray, right: PyArray) -> PyArray:
-    # In C backend, output is given as first input
-    output = zeros(left.shape)
-    left_c_1_ptr = ctypes.cast(left.arr.data, ctypes.POINTER(ctypes.c_float))
-    left_c = PyArray(Array(data=left_c_1_ptr), left.shape)
-    right_c_1_ptr = ctypes.cast(right.arr.data, ctypes.POINTER(ctypes.c_float))
-    right_c = PyArray(Array(data=right_c_1_ptr), right.shape)
-    lib.add(
-        ctypes.byref(output.arr), ctypes.byref(left_c.arr), ctypes.byref(right_c.arr)
-    )
-    _shape = output.shape
-    data_ptr = ctypes.cast(output.arr.data, ctypes.c_void_p)
-    return PyArray(ggml_struct(data=data_ptr), _shape)
+    return binary_op(left, right, lib.add)
 
 
 def multiplication(left: PyArray, right: PyArray) -> PyArray:
+    return binary_op(left, right, lib.multiplication)
+
+
+def binary_op(
+    left: PyArray,
+    right: PyArray,
+    op: Callable[
+        [ctypes.POINTER(Array), ctypes.POINTER(Array), ctypes.POINTER(Array)], None  # type: ignore
+    ],
+) -> PyArray:
     # In C backend, output is given as first input
     output = zeros(left.shape)
     left_c_1_ptr = ctypes.cast(left.arr.data, ctypes.POINTER(ctypes.c_float))
     left_c = PyArray(Array(data=left_c_1_ptr), left.shape)
     right_c_1_ptr = ctypes.cast(right.arr.data, ctypes.POINTER(ctypes.c_float))
     right_c = PyArray(Array(data=right_c_1_ptr), right.shape)
-    lib.multiplication(
-        ctypes.byref(output.arr), ctypes.byref(left_c.arr), ctypes.byref(right_c.arr)
-    )
+    op(ctypes.byref(output.arr), ctypes.byref(left_c.arr), ctypes.byref(right_c.arr))
     _shape = output.shape
     data_ptr = ctypes.cast(output.arr.data, ctypes.c_void_p)
     return PyArray(ggml_struct(data=data_ptr), _shape)
